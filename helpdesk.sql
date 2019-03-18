6a7e6e331f19493cc78d4e412bb06e14bbc85be4
bdaubry <bdaubry@gmail.com>
2019-03-18 16:28:59 -0500

    adding sql file to show commands to create database so I can update between computers

---
+++ helpdesk.sql
@@ -0,0 +1,254 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:8889
+-- Generation Time: Mar 18, 2019 at 09:27 PM
+-- Server version: 5.7.24-log
+-- PHP Version: 7.2.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `helpdesk`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `persistent_logins`
+--
+
+CREATE TABLE `persistent_logins` (
+  `username` varchar(64) NOT NULL,
+  `series` varchar(64) NOT NULL,
+  `token` varchar(64) NOT NULL,
+  `last_used` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `role`
+--
+
+CREATE TABLE `role` (
+  `role_id` int(11) NOT NULL,
+  `role` varchar(255) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `role`
+--
+
+INSERT INTO `role` (`role_id`, `role`) VALUES
+(1, 'ADMIN'),
+(2, 'USER');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `status`
+--
+
+CREATE TABLE `status` (
+  `status_id` int(11) NOT NULL,
+  `author` varchar(255) DEFAULT NULL,
+  `description` varchar(255) NOT NULL,
+  `title` varchar(50) NOT NULL,
+  `update_date` datetime DEFAULT CURRENT_TIMESTAMP
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `status`
+--
+
+INSERT INTO `status` (`status_id`, `author`, `description`, `title`, `update_date`) VALUES
+(1, 'admin@admin.com', 'this is a ticket update to an already open ticket', 'this is a ticket update', NULL),
+(2, 'admin@admin.com', 'this is a second update to test adding status updates', 'this is a second update', NULL),
+(3, 'admin@admin.com', 'this is the status update on a third ticket that was created', 'this is a status update on ticket 3', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ticket`
+--
+
+CREATE TABLE `ticket` (
+  `id` int(11) NOT NULL,
+  `date_closed` datetime DEFAULT NULL,
+  `date_opened` datetime DEFAULT NULL,
+  `description` varchar(500) NOT NULL,
+  `title` varchar(50) NOT NULL,
+  `assigned_to_id` int(11) DEFAULT NULL,
+  `created_by_id` int(11) DEFAULT NULL,
+  `status` varchar(255) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `ticket`
+--
+
+INSERT INTO `ticket` (`id`, `date_closed`, `date_opened`, `description`, `title`, `assigned_to_id`, `created_by_id`, `status`) VALUES
+(1, NULL, '2019-03-12 23:51:03', 'test ticket 1', 'this is a test ticket', NULL, 2, NULL),
+(2, NULL, '2019-03-12 23:56:32', 'test ticket 2', 'this is a second test ticket', NULL, 2, NULL),
+(3, NULL, '2019-03-16 15:33:58', 'third ticket description goes here', 'this is a third ticket', NULL, 2, NULL),
+(4, NULL, '2019-03-16 16:17:42', 'this is a description', 'this is a title that is meant to be way too long', NULL, 2, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ticket_status`
+--
+
+CREATE TABLE `ticket_status` (
+  `ticket_id` int(11) NOT NULL,
+  `status_id` int(11) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `ticket_status`
+--
+
+INSERT INTO `ticket_status` (`ticket_id`, `status_id`) VALUES
+(2, 1),
+(2, 2),
+(3, 3);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `active` int(11) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `firstname` varchar(255) DEFAULT NULL,
+  `lastname` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `active`, `email`, `firstname`, `lastname`, `password`) VALUES
+(1, 1, 'bdaubry@gmail.com', 'Brian', 'Aubry', '$2a$10$.RUw5S/zyl/HGBd85o6Q/e0hnXsYyAnVzJILHTqPPkDqmlMWLPxJu'),
+(2, 1, 'admin@admin.com', 'admin', 'admin', '$2a$10$fD1BeYqFJntdYpE9.gNVquAAEzJxHNgqczYKGwQcn18Ngvnwt/aUO'),
+(3, 1, 'testuser@test.com', 'testuser', 'testuser', '$2a$10$aUGCFq11bHq/XPr4eN0sG.0z9AlX3c5OdjsMgjPjH5oYJTK2hIsFa');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_role`
+--
+
+CREATE TABLE `user_role` (
+  `user_id` int(11) NOT NULL,
+  `role_id` int(11) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `user_role`
+--
+
+INSERT INTO `user_role` (`user_id`, `role_id`) VALUES
+(1, 2),
+(2, 1),
+(2, 2),
+(3, 2),
+(11, 2);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `persistent_logins`
+--
+ALTER TABLE `persistent_logins`
+  ADD PRIMARY KEY (`series`);
+
+--
+-- Indexes for table `role`
+--
+ALTER TABLE `role`
+  ADD PRIMARY KEY (`role_id`);
+
+--
+-- Indexes for table `status`
+--
+ALTER TABLE `status`
+  ADD PRIMARY KEY (`status_id`);
+
+--
+-- Indexes for table `ticket`
+--
+ALTER TABLE `ticket`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `FKfatbq9qkusl0yex88521x9fk6` (`assigned_to_id`),
+  ADD KEY `FKbot3adlibnkqwfrh38v72jjo1` (`created_by_id`);
+
+--
+-- Indexes for table `ticket_status`
+--
+ALTER TABLE `ticket_status`
+  ADD KEY `FK6yn1r2tmmlbdwhseesabht9o8` (`status_id`),
+  ADD KEY `FKc5crr2kjup6so4cfoslpc0a5l` (`ticket_id`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `user_role`
+--
+ALTER TABLE `user_role`
+  ADD PRIMARY KEY (`user_id`,`role_id`),
+  ADD KEY `FKa68196081fvovjhkek5m97n3y` (`role_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `role`
+--
+ALTER TABLE `role`
+  MODIFY `role_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `status`
+--
+ALTER TABLE `status`
+  MODIFY `status_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `ticket`
+--
+ALTER TABLE `ticket`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
